81258224df3d603a85721f45fc8cde9b409541c2
p4kz <wolpak@hotmail.com>
2022-08-18 04:11:34 -0300

    first test conversion and format number

--- mon-conv/src/GUI.java
+++ mon-conv/src/GUI.java
@@ -1,7 +1,9 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -9,45 +11,71 @@ import javax.swing.JTextField;
 
 public class GUI implements ActionListener {
 
+	private static JLabel userValuelabel;
+	private static JTextField userValueInput;
+	private static JLabel result;
+	private static JButton buttonConverter;
+	private static JComboBox<String> comboBox;
+
 	public static void main(String[] args) {
 		JPanel panel = new JPanel();
 		JFrame frame = new JFrame();
-		
+
 		// Window config
-		frame.setSize(310, 200);
+		frame.setSize(310, 175);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
-		
+
 		panel.setLayout(null);
-		
+
 		// Create label
-		JLabel userLabel = new JLabel("Valor para ser convertido");
-		userLabel.setBounds(10, 20, 250, 25);
-		panel.add(userLabel);
-		
+		userValuelabel = new JLabel("Valor para ser convertido");
+		userValuelabel.setBounds(10, 10, 250, 25);
+		panel.add(userValuelabel);
+
 		// Create input
-		JTextField userText = new JTextField(20);
-		userText.setBounds(200, 20, 100, 25);
-		panel.add(userText);
-		
+		userValueInput = new JTextField(20);
+		userValueInput.setBounds(200, 10, 100, 25);
+		panel.add(userValueInput);
+
+		// Create checkbox
+		String convertOptions[] = { "De BRL/Real para USD/Dolar", "De USD/Dolar para BRL/Real", };
+
+		comboBox = new JComboBox<>(convertOptions);
+		comboBox.setBounds(10, 45, 290, 30);
+		panel.add(comboBox);
+
 		// Create label result
-		JLabel result = new JLabel("");
-		result.setBounds(10, 50, 290, 25);
+		result = new JLabel("");
+		result.setBounds(10, 75, 290, 25);
 		panel.add(result);
-		
+
 		// Create button converter
-		JButton buttonConverter = new JButton("Converter");
-		buttonConverter.setBounds(10, 80, 290, 25 );
+		buttonConverter = new JButton("Converter");
+		buttonConverter.setBounds(10, 100, 290, 25);
 		panel.add(buttonConverter);
-		
+
 		buttonConverter.addActionListener(new GUI());
-		
 
 		frame.setVisible(true);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("Click");
+		String userText = userValueInput.getText();
+		double userValue = Double.parseDouble(userText);
+
+		double USDPrice = 5.17;
+
+		if (comboBox.getSelectedItem() == "De BRL/Real para USD/Dolar") {
+			double calc = userValue / USDPrice;
+			result.setText(String.valueOf("$ " + new DecimalFormat("#.##").format(calc)));
+		}
+
+		if (comboBox.getSelectedItem() == "De USD/Dolar para BRL/Real") {
+			double calc = userValue * USDPrice;
+			result.setText(String.valueOf("R$ " + new DecimalFormat("#.##").format(calc)));
+		}
+
 	}
 
 }
